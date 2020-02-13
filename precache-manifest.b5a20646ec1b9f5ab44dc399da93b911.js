1835aa41091092afc08fcc7de86b29f07bddc7cb
Patrick Leonhardt <patrick.leonhardt@gmx.net>
2020-02-13 15:17:39 +0100

    Updates

---
+++ precache-manifest.b5a20646ec1b9f5ab44dc399da93b911.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067aaa56230d6b32f27f84795914cbc1",
+    "url": "/Pathfinding-Visualizer/index.html"
+  },
+  {
+    "revision": "9471e3a83e662e26a744",
+    "url": "/Pathfinding-Visualizer/static/css/main.4cdba946.chunk.css"
+  },
+  {
+    "revision": "08e2ebd238fcdd9354a8",
+    "url": "/Pathfinding-Visualizer/static/js/2.26ed091e.chunk.js"
+  },
+  {
+    "revision": "596e4823497e45004d2db4c50beba3f3",
+    "url": "/Pathfinding-Visualizer/static/js/2.26ed091e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9471e3a83e662e26a744",
+    "url": "/Pathfinding-Visualizer/static/js/main.532e5ff7.chunk.js"
+  },
+  {
+    "revision": "dc3f9efbff2ee309663c",
+    "url": "/Pathfinding-Visualizer/static/js/runtime-main.12b35b00.js"
+  },
+  {
+    "revision": "baed874374c061055f7794dfa7144651",
+    "url": "/Pathfinding-Visualizer/static/media/goal.baed8743.svg"
+  },
+  {
+    "revision": "6ad1fbe824ee8b09f5e61b81229cc1df",
+    "url": "/Pathfinding-Visualizer/static/media/start.6ad1fbe8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pathfinding-visualizer/precache-manifest.fe11f3cd8c904aba3f79bff6ceeae47b.js"
+  "/Pathfinding-Visualizer/precache-manifest.b5a20646ec1b9f5ab44dc399da93b911.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pathfinding-visualizer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Pathfinding-Visualizer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
